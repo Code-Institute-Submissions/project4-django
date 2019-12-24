d7f3428df84c96ca02ee3623b7f65439b4bfc397
Ubuntu <ubuntu@ip-172-31-42-27.ec2.internal>
2019-12-24 09:27:46 +0000

    added login link and function

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,40 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.contrib import auth, messages
+from .forms import UserLoginForm
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
     
+
+def login(request):
+    """Returns the login page"""
+    if request.method == 'POST':
+        login_form = UserLoginForm(request.POST) # populate the form from what the user has keyed in
+        if login_form.is_valid():
+            # attempt to check the username and password is valid
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password'])
+            messages.success(request, "You have successfully logged in")
+            if user:
+                # log in the user
+                auth.login(user=user, request=request)
+                return redirect(reverse('index'))
+            else:
+                login_form.add_error(None, "Invalid username or password")
+                return render(request, 'login.html', {
+                  'form':login_form
+                })
+
+    else:
+        login_form = UserLoginForm()
+        return render(request, 'login.html', {
+            'form':login_form
+        })
+
 def logout(request):
     auth.logout(request)
     messages.success(request, "You have successfully been logged out")
     return redirect(reverse('index'))
+    
+    
