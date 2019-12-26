0bbcd3b3d9daca46cab50c274fc8e63fb40e608e
Ubuntu <ubuntu@ip-172-31-42-27.ec2.internal>
2019-12-26 17:02:27 +0000

    added add function

--- job_listing/views.py
+++ job_listing/views.py
@@ -11,19 +11,19 @@ def show_listing(request):
     })
 
 def add_listing(request):
-    # if request.method == "POST":
-    #     new_listing = Newjob(request.POST)
-    #     if new_listing.is_valid():
-    #         instance = new_listing.save(commit=False)
-    #         instance.user = request.user
-    #         instance.save()
-    #     return redirect(show_listing)
+    if request.method == "POST":
+        new_listing = Newjob(request.POST)
+        if new_listing.is_valid():
+            instance = new_listing.save(commit=False)
+            instance.user = request.user
+            instance.save()
+        return redirect(show_listing)
         
-    # else : 
-    new_listing = Newjob()
-    return render(request, 'add_listing.html',{
-        'form' : new_listing
-        })
+    else : 
+        new_listing = Newjob()
+        return render(request, 'add_listing.html',{
+            'form' : new_listing
+            })
 
 def edit_listing(request):
     return render(request, 'edit_listing.html')
